3cc480c160d2c883a23cb56365b68f797540b307
kedour <10920085@qq.com>
2020-04-02 16:51:59 +0800

    新增service

---
+++ day02_mybatisCRUD/src/main/java/com/itmybatis/service/Impl/UserserviceImpl.java
@@ -0,0 +1,13 @@
+package com.itmybatis.service.Impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ClassName UserserviceImpl
+ * @Description
+ * @Author like
+ * @Date 2020/4/2 16:50
+ * @Version v1.0
+ */
+public class UserserviceImpl {
+}
---
+++ day02_mybatisCRUD/src/main/java/com/itmybatis/service/Userservice.java
@@ -0,0 +1,13 @@
+package com.itmybatis.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @ClassName Userservice
+ * @Description
+ * @Author like
+ * @Date 2020/4/2 16:51
+ * @Version v1.0
+ */
+public interface Userservice {
+}
